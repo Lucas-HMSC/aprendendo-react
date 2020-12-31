d56caac4f7ba893bd8b488ffa63c89c441ae2c74
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-12-31 18:43:14 -0300

    Component Radio.js

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('Verde');
+  const [frutas, setFrutas] = React.useState('');
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Verde']} value={cor} setValue={setCor} />
+      <h2>Frutas</h2>
+      <Radio
+        options={['Limão', 'Laranja', 'Uva']}
+        value={frutas}
+        setValue={setFrutas}
+      />
       <Select
         options={['Smartphone', 'Tablet']}
         value={produto}
