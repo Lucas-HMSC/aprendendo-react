61361cdc748eb98b3e0173f1a00e31fd431ff69d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-01 13:20:49 -0300

    Create ex014

---
+++ exercises/ex014/NaoEncontrado.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NaoEncontrado = () => {
+  return (
+    <div>
+      <p>Erro: 404 - Página Não Encontrada.</p>
+    </div>
+  );
+};
+
+export default NaoEncontrado;
