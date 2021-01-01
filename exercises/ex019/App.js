785f26165518e94a9251ec0cc6fd63c1198c13fc
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-01 17:56:59 -0300

    Create ex019

---
+++ exercises/ex019/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Produtos from './Components/Produtos';
+import Contato from './Components/Contato';
+import Produto from './Components/Produto';
+
+// Utilize a API abaixo para puxar a lista de produto
+// https://ranekapi.origamid.dev/json/api/produto
+// Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="/produto/:id" element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
