38487197d8828417e9e835fc5459c92f9c7e6ca7
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-01-04 17:26:34 -0300

    Update UserStatsGraphs.js

--- dogs/src/Components/User/UserStatsGraphs.js
+++ dogs/src/Components/User/UserStatsGraphs.js
@@ -43,7 +43,7 @@ const UserStatsGraphs = ({ data }) => {
           }}
         />
       </div>
-      <div className={`${styles.graphItem}`}>
+      <div className={styles.graphItem}>
         <VictoryChart>
           <VictoryBar alignment="start" data={graph}></VictoryBar>
         </VictoryChart>
